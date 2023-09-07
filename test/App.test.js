b8719c73b14ba990140ca310ad85d4156ffb4d50
sdlcz <132066587+sdlcz@users.noreply.github.com>
2023-09-07 15:24:12 +0100

    Add test

--- test/App.test.js
+++ test/App.test.js
@@ -4,6 +4,12 @@ import App from "../src/App";
 const gitHub = require("../src/App");
 const expect = require("chai").expect;
 
+describe("App Component", () => {
+  it("renders without error", () => {
+    render(<App />);
+  });
+});
+
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
